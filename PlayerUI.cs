b4975783d2f5572cb62ad48c391e7530d2758011
Rex6525644563456 <164010941+Rex6525644563456@users.noreply.github.com>
2024-04-11 12:39:01 +0200

    Add files via upload

---
+++ PlayerUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class PlayerUI : MonoBehaviour
+
+    public Player player;
+    public TextMeshProUGUI coinscounterText;
+    public Slider health Slider; 
+    
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
